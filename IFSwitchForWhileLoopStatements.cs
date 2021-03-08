5525a7089760203a4b5b51acfad3a8ce2587885e
https://github.com/naimul3070/egen.git <16103070@iubat.edu>
2021-03-08 23:26:28 +0600

    @

---
+++ IFSwitchForWhileLoopStatements.cs
@@ -0,0 +1,57 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace NaimulIsam
+//{
+//    class IFSwitchForWhileLoopStatements
+//    {
+//        static void Main(string[] args)
+//        //for loop
+//        {
+//            for (Int32 i=0;i<3;i++)
+//            {
+//                Console.WriteLine(i);
+//            }
+//            //Int32 value = 3, i = 0;
+
+//            //while (i < value)
+//            //{
+//            //    Console.WriteLine(i);
+//            //    i = i + 1;
+//            //}
+
+//            Console.ReadKey();
+//        }
+
+//    }
+
+
+//        ////brake statment
+//        //{
+//        //    Int32 value = 11;
+//        //    switch (value)
+//        //    {
+//        //        case 1: Console.WriteLine("Value is 1");
+//        //            break;
+//        //        case 2: Console.WriteLine("Value is 2");
+//        //            break;
+//        //        default: Console.WriteLine("Value is different");
+//        //            break;
+//        //    }
+
+//        //if else statement
+
+//        //if (value > 10)
+//        //{
+//        //    Console.WriteLine("value is less than 10");
+//        //}
+//        //else
+//        //{
+//        //    Console.WriteLine("Value is greater than 10");
+//        //}
+
+//        ////Console.ReadKey();
+    
+//}
+
