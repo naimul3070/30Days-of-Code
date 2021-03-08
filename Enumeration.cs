a6cf6abf6bbcfec07b429920eda42c8f4aa9e627
https://github.com/naimul3070/egen.git <16103070@iubat.edu>
2021-03-08 22:59:42 +0600

    variabl guru 99

--- Enumeration.cs
+++ Enumeration.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Threading;
 
-namespace NaimulIsam
-{
-    class Enumeration
-    {
-        enum Days { sun, mon, tues, wed, thursday, friday };
+//namespace NaimulIsam
+//{
+//    class Enumeration
+//    {
+//        enum Days { sun, mon, tues, wed, thursday, friday };
 
-        static void Main(string[] args)
-        {
-            Console.Write(Days.sun);
-            Console.ReadKey();
-        }
-    }
-}
+//        static void Main(string[] args)
+//        {
+//            Console.Write(Days.sun);
+//            Console.ReadKey();
+//        }
+//    }
+//}
---
+++ variablesand operators.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace NaimulIsam
+//{
+//    class variablesand_operators
+//    {
+//        static void Main(string[] args)
+//        {
+//            //string clock = "The Total Amount is ";
+//            //Int32 amount= 50;
+
+//            //Console.WriteLine(clock + amount);
+//            //Console.ReadKey();
+            
+//            //another type of variable
+
+//            Int32 val1 = 10, val2 = 20;
+//            bool status = true;
+
+//            Console.WriteLine(val1 + val2);
+//            Console.WriteLine(val1 < val2);
+//            Console.WriteLine(!(status));
+//            Console.ReadKey();
+
+//        }
+//    }
+//}
