628271da423223e01d609fabeef66150f13ecaae
Naimul Islam <naimul3070@hotmail.com>
2021-03-08 17:40:37 +0600

    2

---
+++ Enumeration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace NaimulIsam
+{
+    class Enumeration
+    {
+        enum Days { sun, mon, tues, wed, thursday, friday };
+
+        static void Main(string[] args)
+        {
+            Console.Write(Days.sun);
+            Console.ReadKey();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,27 +1,43 @@
-﻿using System;
+﻿//using System;
 
-namespace NaimulIsam
-{
-    class Program
-    {
-        static void Main(String[]args)
-        {
-            int i = 4;
-            double d = 4.0;
-            string s = "HackerRank";
+//namespace NaimulIsam
+//{
+//    class Program
+//    {
+//        static void Main(String[] args)
+//            //{
+//            //    int i = 4;
+//            //    double d = 4.0;
+//            //    string s = "HackerRank";
 
-            int i2;
-            double d2;
-            string s2;
+//            //    int i2;
+//            //    double d2;
+//            //    string s2;
 
-            i2 = int.Parse(Console.ReadLine());
-            d2 = double.Parse(Console.ReadLine());
-            s2 = Console.ReadLine();
+//            //    i2 = int.Parse(Console.ReadLine());
+//            //    d2 = double.Parse(Console.ReadLine());
+//            //    s2 = Console.ReadLine();
 
-            Console.WriteLine(String.Format("{0:0}", i + i2));
-            Console.WriteLine(String.Format("{0:0.0}", d + d2));
-            Console.WriteLine(s + s2);
+//            //    Console.WriteLine(String.Format("{0:0}", i + i2));
+//            //    Console.WriteLine(String.Format("{0:0.0}", d + d2));
+//            //    Console.WriteLine(s + s2);
 
-        }
-    }
-}
+//            //}
+//        {
+//            int a = 100;
+//            int b = 50;
+
+//            int sum = a + b;
+//            int sub = a - b;
+//            int mul = a * b;
+//            int div = a / b;
+//            int mod = a % b;
+//            int incriment = a++;
+//            int decrement = b--;
+
+//            a += 10;
+//            a -= 5;
+
+//        }
+//    }
+//}
