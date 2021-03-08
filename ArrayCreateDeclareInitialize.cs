8a1ce6cc817ff06811be72c243faee5c6ac79ee7
https://github.com/naimul3070/egen.git <16103070@iubat.edu>
2021-03-08 23:39:35 +0600

    ok

---
+++ ArrayCreateDeclareInitialize.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace NaimulIsam
+//{
+//    //Array
+//    class ArrayCreateDeclareInitialize
+//    {
+//        static void Main (string[]args)
+//        {
+//            int[] values;
+//            values = new int[3];
+
+//            values[0] = 1;
+//            values[1] = 2;
+//            values[2] = 3;
+
+//            Console.WriteLine(values[0]);
+//            Console.WriteLine(values[1]);
+//            Console.WriteLine(values[2]);
+
+//            Console.ReadKey();
+//        }
+//    }
+//}
