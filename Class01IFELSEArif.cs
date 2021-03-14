8889336687bbfdb5b6f9d023a6c796e24e1e215b
https://github.com/naimul3070/egen.git <16103070@iubat.edu>
2021-03-14 23:29:26 +0600

    IF ELse PRacrtise

---
+++ Class01IFELSEArif.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaimulIsam
+{
+    class Class01IFELSEArif
+    {
+        static void Main(string[] args)
+        {
+            int Mark;
+            //int a;
+
+            Console.WriteLine("Enter Mark");
+            Mark = Convert.ToInt32(Console.ReadLine());
+
+            if (Mark > 100)
+            {
+                Console.WriteLine("Result Not valid");
+            }
+            else
+            {
+                //{
+                //    if (Mark < 0)
+                //    {
+                //        Console.WriteLine("Result is also not valid");
+                //    }
+                //    else
+                //    {
+                //        if(Mark >= 80)
+                //        {
+                //            Console.WriteLine("A+");
+                //        }
+                //        else
+                //        {
+                //            if(Mark >= 70)
+                //            {
+                //                Console.WriteLine("A");
+                //            }
+                //            else
+                //            {
+                //                if (Mark >= 60)
+                //                {
+                //                    Console.WriteLine("B+");
+                //                }
+                //                else
+                //                {
+                //                    if(Mark >= 50)
+                //                    {
+                //                        Console.WriteLine("B");
+                //                    }
+                //                    else
+                //                    {
+                //                        Console.WriteLine("F+");
+                //                    }
+                //                }
+                //            }
+                //        }
+                //    }
+                //}
+
+
+
+            }if (Mark < 0)
+            {
+                Console.WriteLine("Vua");
+            }
+            else if (Mark  >= 80)
+            {
+                Console.WriteLine("A+");
+            }
+            else if (Mark >= 70)
+            {
+                Console.WriteLine("A");
+            }
+            else if (Mark >= 60)
+            {
+                Console.WriteLine("B+");
+            }
+            else if (Mark >= 50)
+            {
+                Console.WriteLine("B");
+            }
+            else if (Mark >= 40)
+            {
+                Console.WriteLine("C");
+            }
+            else if (Mark >= 33)
+            {
+                Console.WriteLine("D");
+            }
+            else
+            {
+                Console.WriteLine("F+");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
