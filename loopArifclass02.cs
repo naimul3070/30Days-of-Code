730d2cd735853f1d6ea680fff7e632834403736a
https://github.com/naimul3070/egen.git <16103070@iubat.edu>
2021-03-15 23:22:30 +0600

    @@

---
+++ loopArifclass02.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace NaimulIsam
+//{
+//    class loopArifclass02
+//    {
+//        static void Main(string[] args)
+//        {
+//            //int Mark;
+
+//            //Console.WriteLine("Enter Mark");
+//            //Mark = Convert.ToInt32(Console.ReadLine());
+
+//            //Mark = Mark - (Mark % 10);
+//            //Console.WriteLine(Mark);
+
+//            //for (int i =1; i <= 10; i++)
+//            //{
+//            //    Console.WriteLine(11-1);
+//            //}
+//            int i = 5;
+
+//            while(i <= 10)
+//            {
+//                Console.WriteLine(i);
+//                i++;
+//            }
+  
+//            Console.ReadKey();
+//        }
+//    }
+//}
