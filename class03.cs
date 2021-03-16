16d76d19b419a183eb789b439691884b0a10317b
https://github.com/naimul3070/egen.git <16103070@iubat.edu>
2021-03-17 01:11:41 +0600

    55

---
+++ class03.cs
@@ -0,0 +1,52 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace NaimulIsam
+//{
+//    class class03
+//    {
+//        static void Main(String[] args)
+//        {
+//            string cont = "y";
+//            while (cont.ToLower() == "y")
+//            {
+//                Console.Clear();
+//                int a, b;
+//                Console.WriteLine("Enter a");
+
+//                try
+//                {
+//                    a = Convert.ToInt32(Console.ReadLine());
+//                }
+//                catch (Exception ex)
+//                {
+//                    Console.WriteLine("Defoult Value of a will : 0\n" + ex.Message);
+//                    a = 0;
+//                }
+//                try
+//                {
+//                    b = Convert.ToInt32(Console.ReadLine());
+//                }
+//                catch (Exception ex)
+//                {
+//                    Console.WriteLine("Defoult Value of b will : 0\n" + ex.Message);
+//                    b = 0;
+//                }
+
+//                Console.WriteLine(a + b);
+
+
+//                Console.WriteLine("Press y to continue, anykety to exit.");
+//                cont = Console.ReadLine();
+//            }
+
+
+
+
+
+//            Console.ReadKey();
+//        }
+//    }
+//}
+
---
+++ oddnumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaimulIsam
+{
+    class oddnumber
+    {
+        static void Main(string[] args)
+        {
+            int a = 57;
+
+            int count = 0;
+            for(int i = 2; i < a; i++)
+            {
+                if (a% 1 == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 0)
+                Console.WriteLine("prime");
+            else
+                Console.WriteLine("Not Prime");
+
+
+            Console.ReadKey();
+        }
+    }
+}
